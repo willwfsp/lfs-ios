ff78c1f8c6e259dae40915355a76aebdda13866d
Pearson <w.policiano@gmail.com>
2016-10-25 01:20:27 -0200

    Unidade 1 - Conteúdo finalizado

---
+++ subprojects/MyFirstProject/MyFirstProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Pearson on 25/10/16.
+//  Copyright © 2016 Pearson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
